46540a2b89dc1dc0cc252fd2e97ea042c157369e
Raphael <rparodi@student.42.fr>
2025-08-27 17:47:12 +0200

    docs(utils/perm): Adding documentation for utils
    - Doxygen will be now required for all the utils

--- src/lib/perm.ts
+++ src/lib/perm.ts
@@ -1,5 +1,10 @@
 import { prisma } from '../lib/prisma.ts';
 
+/**
+ * @param userId - Discord identifier for the user
+ * @param guildId - Discord identifier for the guild
+ * @returns true if the user is whitelisted flase overwise
+ */
 export async function isWhitelisted(userId: string, guildId: string): Promise<boolean> {
 	const userData: User = await prisma.user.findUnique({
 		where: {
