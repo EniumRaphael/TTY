53773e76f56530fcafbe6b5094fe566b83de3836
Raphael <rparodi@student.42.fr>
2025-09-29 20:34:11 +0200

    feat(event/messages): adding the MessageDelete logs

---
+++ src/events/messages/messageDelete.ts
@@ -0,0 +1,40 @@
+import { Events, EmbedBuilder, Message } from 'discord.js';
+import { prisma } from '../../lib/prisma.ts';
+import { Guild as GuildPrisma } from '@prisma/client';
+
+export default {
+	name: Events.MessageDelete,
+	async execute(message: Message) {
+		const guildData: GuildPrisma = await prisma.guild.findUnique({
+			where: {
+				id: message.guildId,
+			},
+		});
+		if (guildData.logMsg) {
+			const log = new EmbedBuilder()
+				.setAuthor({
+					name: `${message.author.tag} (${message.author.id})`,
+					iconURL: message.author.displayAvatarURL({
+						size: 2048,
+						extension: 'png',
+					}),
+				})
+				.setColor(guildData.color)
+				.setFooter({
+					text: guildData.footer,
+				})
+				.setDescription(`
+					Channel: ${message.channel}
+					Content: ${message.content ? message.content : 'enable to load the content'}
+				`);
+			const logChannel = await message.guild.client.channels
+				.fetch(guildData.logMsg)
+				.catch((err) => console.error(err));
+			logChannel.send({
+				embeds: [
+					log,
+				],
+			});
+		}
+	},
+};
