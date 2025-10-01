2df8d6600a83c9136ca3bf67af8c7a36b58a196f
Raphael <rparodi@student.42.fr>
2025-10-01 14:17:07 +0200

    feat(src/lib): adding the a function to choose the correction mention

---
+++ src/lib/mention.ts
@@ -0,0 +1,16 @@
+export function getCorrectMention(guild: Guild, id: string): string {
+	if (id === guild.id) {
+		return '@everyone';
+	}
+
+	const role = guild.roles.cache.get(id);
+	if (role) {
+		return `<@&${id}>`;
+	}
+
+	const member = guild.members.cache.get(id);
+	if (member) {
+		return `<@${id}>`;
+	}
+	return `Unknown (${id})`;
+}
