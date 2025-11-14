7b365660f53f8746084e1391dbb19f25e5d88524
Raphael <rparodi@student.42.fr>
2025-11-14 22:10:13 +0100

    feat(lib/client): now client is extern to be called on all function needed

---
+++ src/lib/client.ts
@@ -0,0 +1,13 @@
+import {
+	Client,
+	GatewayIntentBits,
+} from 'discord.js';
+
+export const client: Client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
