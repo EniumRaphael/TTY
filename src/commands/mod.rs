cc7369bcffe82877acdf6af2563f3612cd8f590a
Raphael <rparodi@student.42.fr>
2026-02-12 16:18:29 +0100

    feat(commands): adding mod.rs

---
+++ src/commands/mod.rs
@@ -0,0 +1,28 @@
+use serenity::all::{
+    CommandInteraction, Context, CreateCommand
+};
+
+include!("./mod_gen.rs");
+
+#[serenity::async_trait]
+pub trait SlashCommand: Send + Sync {
+    fn name(&self) -> &'static str;
+    fn description(&self) -> &'static str;
+
+    fn register(&self) -> CreateCommand;
+
+    async fn run(&self, ctx: &Context, command: &CommandInteraction) -> Result<(), serenity::Error>;
+}
+
+pub struct CommandEntry {
+    pub create: fn() -> Box<dyn SlashCommand>,
+}
+
+inventory::collect!(CommandEntry);
+
+pub fn all_commands() -> Vec<Box<dyn SlashCommand>> {
+    inventory::iter::<CommandEntry>
+        .into_iter()
+        .map(|entry| (entry.create)())
+        .collect()
+}
