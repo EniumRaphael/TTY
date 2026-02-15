a104cbf4535b474a11a48dcf66c13c673e87dd01
Raphael <rparodi@student.42.fr>
2026-02-15 23:42:24 +0100

    feat(events/bot): adding emoji field

--- src/events/bot/interaction_create.rs
+++ src/events/bot/interaction_create.rs
@@ -1,6 +1,7 @@
 use serenity::all::*;
 use sqlx::PgPool;
 use crate::commands::SlashCommand;
+use crate::config::EmojiConfig;
 use crate::events::{BotEvent, EventEntry};
 
 pub struct InteractionHandler;
@@ -9,13 +10,13 @@ pub struct InteractionHandler;
 impl BotEvent for InteractionHandler {
     fn event_type(&self) -> &'static str { "interaction_create" }
 
-    async fn on_interaction_create(&self, ctx: &Context, interaction: &Interaction, commands: &[Box<dyn SlashCommand>], db: &PgPool) {
+    async fn on_interaction_create(&self, ctx: &Context, interaction: &Interaction, commands: &[Box<dyn SlashCommand>], db: &PgPool, emoji: &EmojiConfig) {
         let Interaction::Command(command) = interaction else { return };
 
         let name: &str = command.data.name.as_str();
         match commands.iter().find(|cmd| cmd.name() == name) {
             Some(cmd) => {
-                if let Err(why) = cmd.run(ctx, command, db).await {
+                if let Err(why) = cmd.run(ctx, command, db, emoji).await {
                     eprintln!("❌ | Error on {name}: {why:?}");
                 }
             }
