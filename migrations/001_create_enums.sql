fb63d0e9fff663e72b32219e4c491079e7ad3974
Raphael <rparodi@student.42.fr>
2026-02-26 13:19:53 +0100

    feat(migrations): adding the enum for langs

--- migrations/001_create_enums.sql
+++ migrations/001_create_enums.sql
@@ -1,2 +1,3 @@
 CREATE TYPE bot_presence AS ENUM ('online', 'idle', 'dnd', 'invisible');
 CREATE TYPE bot_activity AS ENUM ('Playing', 'Streaming', 'Listening', 'Watching', 'Competing');
+CREATE TYPE guild_lang AS ENUM ('en', 'fr');
